7c6cb4f867ee75b22117a0b1ac804bdf59905e9c
jamalansari84 <jamalansari84@gmail.com>
2019-12-16 18:11:14 -0500

    colors added JA1

--- packages/common/src/theme/saasModern/colors.js
+++ packages/common/src/theme/saasModern/colors.js
@@ -2,20 +2,20 @@ const colors = {
   transparent: 'transparent', // 0
   black: '#000000', // 1
   white: '#ffffff', // 2
-  headingColor: '#302b4e',
-  textColor: '#43414e', // 3
-  labelColor: '#767676', // 4
-  inactiveField: '#f2f2f2', // 5
-  inactiveButton: '#b7dbdd', // 6
-  inactiveIcon: '#EBEBEB', // 7
-  primary: '#2aa275', // 8
-  primaryHover: '#2aa275', // 9
-  secondary: '#edcd37', // 10
-  secondaryHover: '#edcd37', // 11
-  yellow: '#fdb32a', // 12
-  yellowHover: '#F29E02', // 13
-  borderColor: '#1b1e25', //14
-  primaryBoxShadow: '0px 8px 20px -6px rgba(42, 162, 117, 0.57)',
+  headingColor: '#302b4e', //       JA--> IDK, Purple Dark Hue
+  textColor: '#43414e', // 3        JA--> IDK, Purple Dark Hue2-->Gradient Use
+  labelColor: '#767676', // 4       JA--> AOK, Dark Slate Gray
+  inactiveField: '#f2f2f2', // 5    JA--> AOK, Light Slate Gray
+  inactiveButton: '#b7dbdd', // 6   JA--> AOK, SEA BlueGreen--> NEED to CHANGE TO ORANGE POP
+  inactiveIcon: '#EBEBEB', // 7     JA--> AOK, Light Slate Gray2 --> NEED to CHANGE TO ORANGE POP
+  primary: '#0782F8', // 8          JA - AOK--> BLUE
+  primaryHover: '#0782F8', // 9     JA - AOK--> BLUE
+  secondary: '#FFAB0D', // 10       JA-->CHANGED TO A MORE Q shade of OrangeGold
+  secondaryHover: '#FFAB0D', // 11  JA-->CHANGED TO A MORE Q shade of OrangeGold
+  yellow: '#fdb32a', // 12          JA-->IDK, need a distinct color, save for later
+  yellowHover: '#F29E02', // 13     JA-->IDK, need a distinct color, save for later-ADDD qblueHover
+  borderColor: '#1b1e25',  //14     JA-dark blue purple sorta
+  primaryBoxShadow: '0px 8px 20px -6px rgba(7, 130, 248, 0.65)', //JA--> ADDED NEW Shading to BLUE
   secondaryBoxShadow: '0px 8px 20px -6px rgba(237, 205, 55, 0.5)',
 };
 
