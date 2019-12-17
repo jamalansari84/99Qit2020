19e7e8d8c64cb74fa28019a02d58ec0e5730c5c4
jamalansari84 <jamalansari84@gmail.com>
2019-12-16 19:05:25 -0500

    saasModern added tp themeindexjs

--- packages/common/src/theme/index.js
+++ packages/common/src/theme/index.js
@@ -1,7 +1,9 @@
 import { appTheme } from './app';
 import { agencyTheme } from './agency';
+import { saasModernTheme } from './saasModern';
 
 export const theme = {
   appTheme,
   agencyTheme,
+  saasModernTheme,
 };
