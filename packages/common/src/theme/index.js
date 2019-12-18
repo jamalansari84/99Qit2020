4df714ccefa68578bba63f962b32fc634afb730d
jamalansari84 <jamalansari84@gmail.com>
2019-12-18 17:22:51 -0500

    first commit

--- packages/common/src/theme/index.js
+++ packages/common/src/theme/index.js
@@ -1,9 +1,7 @@
 import { appTheme } from './app';
-import { agencyTheme } from './agency';
 import { saasModernTheme } from './saasModern';
 
 export const theme = {
   appTheme,
-  agencyTheme,
   saasModernTheme,
 };
